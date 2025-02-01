30eb48283661f728b9def84bc6ba2448da5a2f07
Your Name <your.email@example.com>
2025-02-01 16:10:01 -0500

    Task 2 - Sales Report

--- cc_4.js
+++ cc_4.js
@@ -9,3 +9,15 @@ let finalAmount = purchaseAmount - discount;
 console.log(`Purchase Amount: $${purchaseAmount}`);
 console.log(`Final amount after discount: $${finalAmount}`);
 // Logging initial purchase amount and final amount after discount if they meet the conditions. In this case the conditions are met!
+
+// Task 2: For Loop
+let sales = [100, 255, 356, 154, 237];
+//declaring an array of sales
+
+let totalSales = 0;
+
+for (let i = 0; i < sales.length; i++) { 
+    totalSales += sales[i]};
+// Using a for loop to help calculate the total sales
+
+console.log(`Total Sales: $${totalSales}`);
